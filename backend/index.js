5d734c6d979c4bf3b0d354c2f86ce8a2f993cf80
yoyobazzi3 <yoyobazzi3@gmail.com>
2025-05-03 15:48:30 -0700

    mess

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const server = app.listen(process.env.PORT, process.env.HOST, () => {
-  console.log(`Server live at http://${process.env.HOST}:${process.env.PORT}/basePage`)
+  console.log(`Server live `)
 })
 
 export default server;
